d0fa2401d21713f2898b31daca115cee5a02773f
TrilokGondwal <trilokgondwal@gmail.com>
2022-10-18 15:46:20 +0530

    Initial commit

---
+++ data.js
@@ -0,0 +1,30 @@
+let songs = [
+{
+    name: 'song 1 ',
+    path: 'assets/music/Song 1.mp3',
+    artist: 'artist1',
+    cover: 'assets/images/cover 1.png'
+},
+
+{
+    name: 'song 4',
+    path: 'assets/music/Song 4.mp3',
+    artist: 'artist4',
+    cover: 'assets/images/cover 4.png'
+},{
+    name: 'song G',
+    path: 'assets/music/Song G.mp3',
+    artist: 'artistG',
+    cover: 'assets/images/cover 4.png'
+},
+{
+    name: 'song T',
+    path: 'assets/music/Song T.mp3',
+    artist: 'artistT',
+    cover: 'assets/images/cover 4.png'
+},
+
+
+
+    
+]
